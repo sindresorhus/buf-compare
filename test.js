daac5519aa7db4bb94ce0e695eee3dc185186e6a
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 14:48:23 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,28 +1,25 @@
-'use strict';
-var assert = require('assert');
-var test = require('ava');
+import test from 'ava';
+
 Buffer.compare = undefined;
-var bufCompare = require('./');
+const m = require('./');
 
-test(function (t) {
+test(t => {
 	// https://github.com/iojs/io.js/blob/19ffb5cf1c50e9d91d0a7a35152d20f175a2385d/test/parallel/test-buffer.js#L1127-L1157
-	var b = new Buffer('a');
-	var c = new Buffer('c');
-	var d = new Buffer('aa');
+	const b = new Buffer('a');
+	const c = new Buffer('c');
+	const d = new Buffer('aa');
 
-	assert.equal(bufCompare(b, c), -1);
-	assert.equal(bufCompare(c, d), 1);
-	assert.equal(bufCompare(d, b), 1);
-	assert.equal(bufCompare(b, d), -1);
-	assert.equal(bufCompare(c, c), 0);
+	t.is(m(b, c), -1);
+	t.is(m(c, d), 1);
+	t.is(m(d, b), 1);
+	t.is(m(b, d), -1);
+	t.is(m(c, c), 0);
 
-	assert.throws(function () {
-		bufCompare(new Buffer(1), 'abc');
+	t.throws(() => {
+		m(new Buffer(1), 'abc');
 	});
 
-	assert.throws(function () {
-		bufCompare('abc', new Buffer(1));
+	t.throws(() => {
+		m('abc', new Buffer(1));
 	});
-
-	t.end();
 });
